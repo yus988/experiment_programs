b5675d7fef703f89c36e6033eb40998b45b42150
Yusuke Yamazaki <z.yusuke.yamazaki@gree.net>
2021-04-11 09:43:49 +0900

    主観実験カウント　ループ化

--- 1st_ToH2020/IllustrateSubjectiveExperiment.m
+++ 1st_ToH2020/IllustrateSubjectiveExperiment.m
@@ -10,9 +10,9 @@ radiulOfCircles = zeros(1, 1);
 radius_coef = 30;%描画のため加速度(g)に掛け合わせる係数
 
 % ラベル描画関連
-imgCell{1,1} = dir('./frontImg/*.png');
-imgCell{2,1} = dir('./backImg/*.png');
-imgCell{3,1} = dir('./sideImg/*.png');
+imgCell{1,1} = dir('./T-frontImg/*.png');
+imgCell{2,1} = dir('./T-backImg/*.png');
+imgCell{3,1} = dir('./T-sideImg/*.png');
 
 for i=1:15
     if i == 1
@@ -47,14 +47,6 @@ for i=1:15
         range = '308:320'; %rangeSide140hz1w
     end
     
-    if rem(1,3) == 1 
-        imgPath = './frontImg/';
-    elseif rem(1,3) == 2
-        imgPath = './backImg/';
-    elseif rem(1,3) == 2
-        imgPath = './sideImg/';
-    end
-    
     for j = 1:2
         if j == 1 
             fileName = 'y_mean.csv';
@@ -62,63 +54,104 @@ for i=1:15
             fileName = 't_mean.csv';
         end
         Mx{i,j} =readmatrix(fileName,'range',range);
-
     end
     
 end
 
 
 %% マーカー（緑）の位置に実験結果を図示    
-Underlayer_img = imread(strcat(imgPath,'Underlayer.png'));
-num_pointArray =  length(imgCell{1,1}) - 1 ; %緑マーカの列の数。imgフォルダの画像数から算出
-c_Annotation = cell(num_pointArray,1); %描画用マーカー座標を入れるセル
-
-for i=1:num_pointArray
-    img = imread(strcat(imgPath, num2str(i), '.png'));
-    % 閾値からマーカーを二値化、重心を求める
-    % greenDetect.mとnoiseReduction.mが必要
-    [BW, masked] = m_greenDetect(img);
-    BW_filtered = m_noiseReduction(BW);
-    % stats = regionprops(BW1_filtered);
-    I = rgb2gray(masked);
-
-    stats = regionprops(BW_filtered, I ,{'Centroid'});
-    tmp_colmun =zeros(size(stats,1),2) ;
-    for k = 1: size(stats,1)
-        centroids = stats(k).Centroid;
-        tmp_colmun(k,1) = centroids(1,1);
-        tmp_colmun(k,2) = centroids(1,2);
-        % X座標を元にグループ分け、別々の行列に重心座標を代入する
+for clothType = 1:2
+    
+    if clothType == 1
+        cloth = 'Y-';
+    elseif clothType == 2
+        cloth = 'T-';
     end
-    %     Ygを基準に並び替え
-    tmp_colmun = sortrows(tmp_colmun, 2);
-    c_Annotation{i,1} =tmp_colmun;
-end
+    
+    for loop = 1:15
+        % 領域の設定
+        if rem(loop,3) == 1 
+            imgPath = strcat('./',cloth ,'frontImg/');
+            targetImgCell = 1;
+            areaName = 'front';
+        elseif rem(loop,3) == 2
+            imgPath = strcat('./',cloth ,'backImg/');
+            targetImgCell = 2;
+            areaName = 'back';
+        elseif rem(loop,3) == 0
+            imgPath = strcat('./',cloth ,'sideImg/');
+            targetImgCell = 3;
+            areaName = 'side';
+        end
+        % 出力の設定
+        if and(loop >= 1, loop <= 3)
+            power = '0.5W';
+        elseif or(loop >= 10, and(loop >= 4, loop <= 6))
+            power = '1W';
+        elseif and(loop >= 7, loop <= 9)
+            power = '2W';
+        end
+        % 周波数の設定
+        if and(loop >= 1, loop <= 9)
+            freq = '20Hz';
+        elseif and(loop >= 10, loop <= 12)
+            freq = '80Hz';
+        elseif and(loop >= 13, loop <= 15)
+            freq = '140Hz';
+        end
 
-%% 各軸ごとの加速度の大きさと数値を図示
+        Underlayer_img = imread(strcat(imgPath,'Underlayer.png'));
+        num_pointArray =  length(imgCell{targetImgCell,1}) - 1 ; %緑マーカの列の数。imgフォルダの画像数から算出
+        c_Annotation = cell(num_pointArray,1); %描画用マーカー座標を入れるセル
 
-Annotation = zeros(1, 3); %c_AnnotationのFloat行列, [マーカーのx座標, y座用, 加速度の大きさr]
-%viscirclesを使うため、重心と円半径をそれぞれ別の行列に代入
-i = 0; %　c_Annotationの数だけ参照行をシフトさせるため
-for m = 1:size(c_Annotation,1)
-    for k = 1: size(c_Annotation{m,1},1)
-        Annotation(k+i, 1) = c_Annotation{m,1}(k, 1);
-        Annotation(k+i, 2) = c_Annotation{m,1}(k, 2);
-        % loop化
-        Annotation(k+i, 3) = Mx{1,1}(k+i,1) * radius_coef; % 半径となる値を代入
+        for i=1:num_pointArray
+            img = imread(strcat(imgPath, num2str(i), '.png'));
+            % 閾値からマーカーを二値化、重心を求める
+            % greenDetect.mとnoiseReduction.mが必要
+            [BW, masked] = m_greenDetect(img);
+            BW_filtered = m_noiseReduction(BW);
+            % stats = regionprops(BW1_filtered);
+            I = rgb2gray(masked);
+
+            stats = regionprops(BW_filtered, I ,{'Centroid'});
+            tmp_colmun =zeros(size(stats,1),2) ;
+            for k = 1: size(stats,1)
+                centroids = stats(k).Centroid;
+                tmp_colmun(k,1) = centroids(1,1);
+                tmp_colmun(k,2) = centroids(1,2);
+                % X座標を元にグループ分け、別々の行列に重心座標を代入する
+            end
+            %     Ygを基準に並び替え
+            tmp_colmun = sortrows(tmp_colmun, 2);
+            c_Annotation{i,1} =tmp_colmun;
+        end
+
+        %% 各軸ごとの加速度の大きさと数値を図示
+
+        Annotation = zeros(1, 3); %c_AnnotationのFloat行列, [マーカーのx座標, y座用, 加速度の大きさr]
+        %viscirclesを使うため、重心と円半径をそれぞれ別の行列に代入
+        i = 0; %　c_Annotationの数だけ参照行をシフトさせるため
+        for m = 1:size(c_Annotation,1)
+            for k = 1: size(c_Annotation{m,1},1)
+                Annotation(k+i, 1) = c_Annotation{m,1}(k, 1);
+                Annotation(k+i, 2) = c_Annotation{m,1}(k, 2);
+                % loop化
+                Annotation(k+i, 3) = Mx{loop,clothType}(k+i,1) * radius_coef; % 半径となる値を代入
+            end
+            i = i + size(c_Annotation{m,1},1);
+        end
+
+        % Annotationの大きさなどを決定
+        annoColor = 'blue';
+        dispFrame = insertObjectAnnotation(Underlayer_img, 'circle', Annotation, Annotation(:,3)/radius_coef, ...
+            'FontSize', 10, 'LineWidth', 2,'TextBoxOpacity',0, 'color', 'magenta','TextColor', annoColor);
+
+        imshow(dispFrame,'Border','tight') % border tight を入れることで余白なしに
+
+        x_base = 0;
+        y_base = 20;
+        title = strcat(cloth,areaName,'-',freq,'-',power);
+        text(x_base,y_base,title,'Color','white','FontSize',12);        
+        saveas(gcf,strcat(title,'.png'));
     end
-    i = i + size(c_Annotation{m,1},1);
 end
-
-% Annotationの大きさなどを決定
-annoColor = 'blue';
-dispFrame = insertObjectAnnotation(Underlayer_img, 'circle', Annotation, Annotation(:,3)/radius_coef, ...
-    'FontSize', 10, 'LineWidth', 2,'TextBoxOpacity',0, 'color', 'magenta','TextColor', annoColor);
-
-imshow(dispFrame,'Border','tight') % border tight を入れることで余白なしに
-
-x_base = 220;
-y_base = 20;
-y_offset = 30;
-text(x_base,y_base,'20Hz','Color','white','FontSize',20);        
-saveas(gcf,strcat('sum','.png'));
